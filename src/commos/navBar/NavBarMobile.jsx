6a9ff9958ec62922f235f01ea11e98a6f922c1bf
Martin Cardozo <tiin.-@hotmail.es>
2023-11-16 15:57:30 -0300

    se crea la segunda version del nav mobile y desktop

---
+++ src/commos/navBar/NavBarMobile.jsx
@@ -0,0 +1,74 @@
+import { NavLink } from "react-router-dom";
+import "./navbar.css";
+const NavBarMobile = () => {
+  return (
+    <header className="header" id="mobileFirst">
+      <nav className=" navbar bg-body-tertiary fixed-bottom border-top ">
+        <ul className="nav-links mx-auto ">
+          <li>
+            <div className="text-center">
+              <NavLink>
+                <i className="bi bi-house fs-2 text-dark "></i>
+              </NavLink>
+              <NavLink
+                className={`nav-link text-dark ${({ isActive }) =>
+                  isActive ? "active" : ""}`}
+                aria-current="page"
+                to="/Home"
+              >
+                Home
+              </NavLink>
+            </div>
+          </li>
+          <li>
+            <div className="text-center ms-5">
+              <NavLink>
+                <i className="bi bi-columns-gap fs-2 text-dark"></i>
+              </NavLink>
+              <NavLink
+                className={`nav-link text-dark ${({ isActive }) =>
+                  isActive ? "active" : " "}`}
+                aria-current="page"
+                to="/"
+              >
+                Menu
+              </NavLink>
+            </div>
+          </li>
+          <li>
+            <div className="text-center ms-5 ">
+              <NavLink>
+                <i className="bi bi-building-fill-down fs-2 text-dark "></i>
+              </NavLink>
+              <NavLink
+                className={`nav-link text-dark ${({ isActive }) =>
+                  isActive ? "active" : ""}`}
+                aria-current="page"
+                to="/"
+              >
+                Pedidos
+              </NavLink>
+            </div>
+          </li>
+          <li>
+            <div className="text-center ms-5  ">
+              <NavLink>
+                <i className="bi bi-person-circle fs-2 text-dark "></i>
+              </NavLink>
+              <NavLink
+                className={`nav-link text-dark ${({ isActive }) =>
+                  isActive ? "activePage" : ""}`}
+                aria-current="page"
+                to="/"
+              >
+                Perfil
+              </NavLink>
+            </div>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBarMobile;
--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,22 @@ import ReactDOM from "react-dom/client";
 
 //Importaciones de librerias
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter , Routes } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css'
+
 //Importaciones de componentes nuestros
-import NavBar from "./commos/NavBar";
+import NavBarMain from "./commos/navBar/navBarMain";
+
+
+
 
 //importaciones de estilos
 import "./index.css";
-import { Prueba } from "./components/Prueba";
+
+
+
 
 
 const queryClient = new QueryClient();
@@ -21,11 +27,13 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
+       <NavBarMain></NavBarMain>
         <main>
-        <Prueba/>
-        <Routes></Routes>
+        <Routes>
+        
+        </Routes>
         </main>
-        <NavBar />
+        
       </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
