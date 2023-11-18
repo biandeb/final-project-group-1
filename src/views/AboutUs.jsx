2f6c9822b0f9fad823336ce10b42d120ff3fa0b4
Bianca Deblinger <biandeb96@gmail.com>
2023-11-18 16:49:12 -0300

    agrego segunda sección

--- src/views/AboutUs.jsx
+++ src/views/AboutUs.jsx
@@ -1,10 +1,11 @@
 import grillImage from '../img/grill2.jpg';
+import grillMeat from '../img/meat.jpg';
 
 import "../styles/aboutUs.css";
 
 const AboutUs = () => {
     return (
-        <section className="text-center position-relative m-0">
+        <section className="text-center position-relative">
         <article>
         <img
             src={grillImage}
@@ -16,21 +17,23 @@ const AboutUs = () => {
         <h2 className="text-light mt-5">WE GRILL • YOU CHILL</h2>
         </div>
         </article>
-        <hr className="text-light" />
-        <div className='bg-light rounded p-4 '>
-          <article className='row'>
-            <div className='col-12 col-md-6 mb-2'>
-            {/* <LoginForm /> */}
+        <article className='container'>
+            <div className='row'>
+        <div className="text-center col-6 background-container">
+        <div className="text-light position-absolute top-15 start-15 translate-middle">
+            <h3 className='fw-bold'>Grill & Thrill are wild about food!</h3>
+            <p className='fw-bold p-one'>Value, quality, and good times are what Grill & Thrill strives for. We prepare, cook, and serve in our unique Wild Way and sincerely hope you enjoy your time with us as much as we appreciate yours. Flame-grilled Steaks, Ribs, and Chicken are our specialities but we also do amazing Calamari, Pizza, and much more. We realise that not everyone is a carnivore, so we continue to expand our menu to suit all. We only use the very best free-range, grass-fed New Zealand Beef available, We carefully selected our Steaks and ensure they are aged until perfect, our Ribs cook overnight and our Chicken marinade for at least 24 hours before being flame-grilled to perfection. Eat delicious food. Grab a drink. But most of all, relax! And please enjoy our little corner of Africa in Auckland. We thank you from the bottom of our hearts for your continued support.</p>
             </div>
-            {/* <div className='col-12 col-md-6'>
-              <img
-                src='https://i.postimg.cc/tgRz5YRm/la-cocinita-de-Eze.jpg'
-                alt='La cocinita de Ezequielito Amin'
-                className='w-75 rounded'
-              />
-            </div> */}
-          </article>
         </div>
+        <div className="text-center col-6">
+        <img
+            src={grillMeat}
+            alt='Grill meat'
+            className='w-100'
+        />
+        </div>
+        </div>
+        </article>
       </section>
       )
 }
