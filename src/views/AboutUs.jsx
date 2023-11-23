6266ada0ae85fbe99ea711f168cf5a66ad4a4061
Bianca Deblinger <biandeb96@gmail.com>
2023-11-23 20:45:14 -0300

    agrego integrantes del grupo en proyecto restaurante

--- src/views/AboutUs.jsx
+++ src/views/AboutUs.jsx
@@ -1,4 +1,7 @@
 import grillImage from "../img/grill3.jpg";
+import bianca from "../img/deblinger-bianca.jpeg";
+import juliana from "../img/elias-juliana.jpeg";
+import martin from "../img/cardozo-martin.jpeg";
 
 import "../styles/aboutUs.css";
 
@@ -25,6 +28,16 @@ const AboutUs = () => {
         <br />
         <hr />
         <p className="grill-text fw-bold">Value, quality, and good times are what Grill & Thrill strives for. We prepare, cook, and serve in our unique Wild Way and sincerely hope you enjoy your time with us as much as we appreciate yours. Flame-grilled Steaks, Ribs, and Chicken are our specialities but we also do amazing Burguers, Pizza, and much more. We realise that not everyone is a carnivore, so we continue to expand our menu to suit all. We only use the very best free-range, grass-fed New Tucson Burguer available. We carefully selected our Steaks and ensure they are aged until perfect, our Burguers cook overnight and our Chicken marinade for at least 24 hours before being flame-grilled to perfection. Eat delicious food. Grab a drink. But most of all, relax! And please enjoy our little corner of America in Argentina. We thank you from the bottom of our hearts for your continued support.</p>
+      </article>
+      {/* Tercera sección */}
+      <article>
+        <div className="back-three">
+            <div className="text-center py-4">
+                <img src={bianca} className="first-member" alt="Bianca is a member from grill and thrill's restaurant" />
+                <img src={juliana} className="second-member" alt="Juliana is a member from grill and thrill's restaurant" />
+                <img src={martin} className="third-member" alt="Martin is a member from grill and thrill's restaurant" />
+            </div>
+        </div>
       </article>
     </>
   );
