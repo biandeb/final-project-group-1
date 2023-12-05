81a48d0ebe390d66ca1672abe595b23923f2e7ce
Martin Cardozo <tiin.-@hotmail.es>
2023-12-04 23:39:26 -0300

    se corrigen estilos

--- src/api/users.js
+++ src/api/users.js
@@ -1,14 +1,14 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const postUserFn = async (data) => {
-  const res = await fetch(`${API_URL}/users`, {
+  const res = await fetch(`${API_URL}/users/`, {
     method: "POST",
     body: JSON.stringify(data),
     headers: {
       "Content-type": "application/json",
     },
   });
-
+  console.log(res);
   if (!res.ok) {
     throw new Error("An error occurred while registering an user");
   }
