06275474b348ec7bc07a6596670597e2a0f75d6b
Martin Cardozo <tiin.-@hotmail.es>
2024-01-10 00:17:02 -0300

    se agrega una ayuda para el usuario en los pedidos

--- src/components/UserView/Products/ProductsGallery.jsx
+++ src/components/UserView/Products/ProductsGallery.jsx
@@ -1,6 +1,8 @@
+import { useSession } from "../../../stores/useSessions";
 import ProductCard from "./ProductCard";
 
 const ProductsGallery = (props) => {
+  const { isLoggedIn } = useSession();
   const { products } = props;
 
   return (
@@ -8,6 +10,11 @@ const ProductsGallery = (props) => {
       <section className="row justify-content-center mb-5">
       <h1 className="fs-1 fw-bold py-4 text-center">Grill & Thrill</h1>
         <h3 className="pb-2 mb-2 text-center">Menu</h3>
+        {!isLoggedIn && (
+          <div className="alert alert-danger text-center fw-bolder" role="alert">
+         LOG IN TO PLACE YOUR ORDER
+        </div>
+        )}
         {products.length === 0 ? <p>No products available</p> : null}
         {products.data
           .filter((product) => product.isAvailable === true)
